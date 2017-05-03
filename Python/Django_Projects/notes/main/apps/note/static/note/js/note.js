cb85fd11bb1814b2109babde0881f249de209a20
Qasim <qasim.zia10@gmail.com>
2017-05-03 13:11:35 -0400

    Finshed Notes

--- Python/Django_Projects/notes/main/apps/note/static/note/js/note.js
+++ Python/Django_Projects/notes/main/apps/note/static/note/js/note.js
@@ -18,13 +18,26 @@ $(document).ready(function() {
         var currentURL = `update/${currentID}`
         var csrf = $($('meta')[1]).attr('content') 
         $(this).replaceWith(`
-        <form action='${currentURL}' method='post'>
+        <form id='edit_note' note_id='${currentID}' action='${currentURL}' method='post'>
             ${csrf}
             <textarea name='update' rows='5' cols='40'>${currentPost}</textarea>
             <br>
             <input type='submit' name='submit'>
         </form>
         `)
+    });
+    $(document).on('submit', '#edit_note' , function(e) {
+        var currentID = $(this).attr('note_id')
+        e.preventDefault();
+        $.ajax({
+            url: `update/${currentID}`,
+            method: 'post',
+            data: $(this).serialize(),
+            success: function(serverResponse){
+                $('.main-content').replaceWith(serverResponse);
+                console.log(serverResponse);
+            }
+        });
     });
     $(document).on('click', '.deleteLink', function(e) {
         e.preventDefault();
