2b3dc543426fb9c8cfe8d17233af08f2658b5366
Qasim <qasim.zia10@gmail.com>
2017-05-08 12:12:29 -0400

    Ruby Initial

---
+++ Ruby/first_vagrant_box/RubyFundamentals/fundamentals.rb
@@ -0,0 +1,49 @@
+=begin
+    this is a long comment box
+=end
+
+#----------------------------------------------------------------------#
+
+# puts "Hello"
+# puts "This is in the middle"
+
+# BEGIN {
+#     puts "This will be implemented first"
+# }
+
+# END {
+#     puts "This is in the end block"
+# }
+
+#----------------------------------------------------------------------#
+
+# x = 5 
+# y = 10
+
+# z = x + y
+
+# puts z
+
+#----------------------------------------------------------------------#
+
+# first_name = "Michael"
+# last_name = "Choi"
+
+# puts "First name is #{first_name} and last name is #{last_name}"
+
+#----------------------------------------------------------------------#
+
+# first_name = "Michael"
+# last_name = "Choi"
+
+# puts "First name is #{first_name} and last name is #{last_name}"
+
+#----------------------------------------------------------------------#
+
+# first_name = "Michael"
+# last_name = "Choi"
+
+# puts "First name is %s and last name is %s" % [first_name, last_name]
+
+#----------------------------------------------------------------------#
+
